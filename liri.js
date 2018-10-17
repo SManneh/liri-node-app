1aa7582626eefeef8dedd094610922ac8b99a450
Nabou <Nabou@Sainabou.local>
2018-10-16 20:21:21 -0500

    console log movie responses

--- liri.js
+++ liri.js
@@ -40,10 +40,19 @@ var spotify = new Spotify({
    }else if(process.argv[2] === 'concert-this'){
 
     var artistName = process.argv.slice(3, process.argv.length).join(' ');
+    if(artistName === ""){
+      console.log("Please Provide Artist or Band Name");
+    }
 
       request(
         `https://rest.bandsintown.com/artists/${artistName}/events?app_id=${keys.band.id}`  , function (error, response, body) {
         
+        if (error) {
+          return console.log('Error occurred: ' + error);
+        }
+        // const infoReceived = JSON.parse(body);
+        // console.log("Venue: ", infoReceived.venue)
+
         console.log('error:', error);
         console.log('statusCode:', response && response.statusCode); 
         console.log('body:', body); 
@@ -52,15 +61,29 @@ var spotify = new Spotify({
   }else if(process.argv[2] === 'movie-this'){
 
     var movieName = process.argv.slice(3, process.argv.length).join(' ');
+    if (movieName === ""){
+      movieName = "Mr. Nobody"
+    }
 
   request(
     `http://www.omdbapi.com/?t=${movieName}&apikey=${keys.movie.id}` , function (error, response, body) {
-  console.log('error:', error); 
-  console.log('statusCode:', response && response.statusCode); 
-  console.log('body:', body); 
+  
+      if (error) {
+        return console.log('Error occurred: ' + error);
+      }
+      const dataReceived = JSON.parse(body);
+      console.log("Movie Title: ", dataReceived.Title );
+      console.log("Year: ", dataReceived.Year );
+      console.log("imdbRating: ", dataReceived.imdbRating );
+      console.log("Rotten Tomatoes: ", dataReceived.Ratings[1].value);
+      console.log("Country Movie Was Produced: ", dataReceived.Country );
+      console.log("Language of The Movie: ", dataReceived.Language );
+      console.log("Plot of The Movie: ", dataReceived.Plot );
+      console.log("Actors: ", dataReceived.Actors );
+
 }
 )
-  }
+  };
 
  
 
